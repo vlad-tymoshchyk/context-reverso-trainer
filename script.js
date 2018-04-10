cf9af03d6907b9ac8d83542ba7f811ff96dabcbe
Suveren1802 <vlsdyslav.sky@gmail.com>
2018-04-10 19:42:34 +0300

    added initial script

---
+++ script.js
@@ -0,0 +1,45 @@
+(function(className, trainingnumber){
+	console.clear();
+	var ex = Array.from(document.querySelectorAll(className));
+	ex.sort(function(){
+		if(Math.random() < 0.5){
+			return -1;
+		} else {
+			return 1;
+		};
+	});
+	var message = messageTwo = messageTree = '-',
+		src = 1,
+		trg = 0,
+		target = '',
+		source = '';
+	if ( /[à-ÿ]/.test(ex[0].children[trg].innerText) ) {
+		src = 0;
+		trg = 1;
+	}
+	for(var i=0, len=trainingnumber; i<len; i++){
+		if(!message) break;
+		source = ex[i].children[src].innerText;
+		target = ex[i].children[trg].innerText;
+		var translation = prompt(source + '\n\n' + (i+1) + '/' + trainingnumber );
+		if(translation == 'stop') {
+			console.warn('Translation has been stoped');
+			break;
+		}
+		messageTree = messageTwo;
+		messageTwo = message;
+		message = source 
+			+ '\n\n' 
+			+ target 
+			+ '\n\n' 
+			+ (translation ? translation : "--------NOT INSERTED---------") 
+			+ '\n-----------------------------\n';
+		console.clear();
+		console.log(message + messageTwo + messageTree);
+		if (i == len-1 ) {
+			console.log('task complited/n==================================');
+		}
+		if(i == len -1 && confirm("Do you want to take it once more???"))
+			i = -1;
+		}
+}(".example", 10))
